d52450ca33ff3d6c3cde8d8703c34046aeb60627
Klayfez <106464183+Klayfez@users.noreply.github.com>
2024-06-11 10:45:33 +0700

    1

--- PotatoClicker/scripts/buy_btn.js
+++ PotatoClicker/scripts/buy_btn.js
@@ -44,7 +44,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const PotatoWorld = document.getElementById("PotatoWorld");
 
     PotatoWorld.addEventListener("mousedown" || "touchstart", function () {
-        count_potatos = count_potatos - 10
+        count_potatos = count_potatos - 10;
         text_count_potatos.textContent = count_potatos;
         localStorage.setItem('count_potatos', count_potatos);
         console.log(count_potatos);
