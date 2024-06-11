5d508047a4108b28793126434f466ce50aac157e
Klayfez <106464183+Klayfez@users.noreply.github.com>
2024-06-11 10:51:50 +0700

    123123

--- PotatoClicker/scripts/buy_btn.js
+++ PotatoClicker/scripts/buy_btn.js
@@ -37,13 +37,14 @@ document.addEventListener('DOMContentLoaded', function () {
 
 document.addEventListener('DOMContentLoaded', function () {
     const text_count_potatos = document.getElementById("text_count_potatos");
-    var count_potatos = localStorage.getItem('count_potatos') ? parseInt(localStorage.getItem('count_potatos'), 10) : 0; // Инициализация переменной
+    
 
     text_count_potatos.textContent = count_potatos; // Обновление значения при загрузке страницы
 
     const PotatoWorld = document.getElementById("PotatoWorld");
 
     PotatoWorld.addEventListener("mousedown" || "touchstart", function () {
+        var count_potatos = localStorage.getItem('count_potatos') ? parseInt(localStorage.getItem('count_potatos'), 10) : 0; // Инициализация переменной
         count_potatos = count_potatos - 10;
         text_count_potatos.textContent = count_potatos;
         localStorage.setItem('count_potatos', count_potatos);
