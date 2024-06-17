00a9a41f745dac6fcf2d82962dbeacf892e5de7b
Sumukin <95268190+Sumukin@users.noreply.github.com>
2024-06-18 00:08:12 +0700

    Update ptt_btn.js

--- PotatoClicker/scripts/ptt_btn.js
+++ PotatoClicker/scripts/ptt_btn.js
@@ -56,7 +56,7 @@ document.addEventListener('DOMContentLoaded', function () {
         } else {
             let options_array = JSON.parse(localStorage.getItem('options_array')) || {};
             keys = Object.keys(options_array)
-            print(keys)
+            alert(keys)
             // lastKey = keys[keys.length - 1]
             console.log(options_array)
             // ptt_btn.src = "images/webp/" + lastKey + "_logo.webp";
