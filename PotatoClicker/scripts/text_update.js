bc0a37fa3a60d809941445fba193c1a171e693b7
Klayfez <106464183+Klayfez@users.noreply.github.com>
2024-06-17 10:56:26 +0700

    1

--- PotatoClicker/scripts/text_update.js
+++ PotatoClicker/scripts/text_update.js
@@ -1,24 +1,31 @@
-export function updatePttsCount() {
+function updatePttsCount() {
     const text_count_ptts = document.getElementById("text_count_ptts");
     let count_ptts = localStorage.getItem('count_ptts') ? parseInt(localStorage.getItem('count_ptts'), 10) : 0
     text_count_ptts.textContent = "🥔 " + count_ptts.toString() + " 🥔";
 }
 
-export function updatePttsIncome() {
+function updatePttsIncome() {
     const text_income_ptts = document.getElementById("text_income_ptts");
     let income = localStorage.getItem('income') ? parseInt(localStorage.getItem('income'), 10) : 0
     text_income_ptts.textContent = "+" + income.toString() + " 🥔/Клик";
 }
 
-export function updateBuyBtnStatus(card_buy_btn_id) {
+function updateBuyBtnStatus(card_buy_btn_id) {
     const card_buy_btn = document.getElementById(card_buy_btn_id);
     card_buy_btn.innerHTML = "Куплено"
 }
 
-export function updateAllBuyBtnsStatus(card_buy_btn_ids) {
+function updateAllBuyBtnsStatus(card_buy_btn_ids) {
     let options_array = JSON.parse(localStorage.getItem('options_array')) || {};
     for (var keys in options_array) {
         var card_buy_btn = document.getElementById(keys);
         card_buy_btn.innerHTML = "Куплено"
     }
+}
+
+export {
+    updatePttsCount,
+    updatePttsIncome,
+    updateBuyBtnStatus,
+    updateAllBuyBtnsStatus
 }
