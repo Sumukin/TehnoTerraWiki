d83cb7cc4d43c2fdb5432bae40d0fa1589fac385
Sumukin <95268190+Sumukin@users.noreply.github.com>
2024-06-17 23:58:45 +0700

    Update ptt_btn.js

--- PotatoClicker/scripts/ptt_btn.js
+++ PotatoClicker/scripts/ptt_btn.js
@@ -36,7 +36,7 @@ document.addEventListener('DOMContentLoaded', function () {
             localStorage.setItem("income", income);
         }
         count_ptts = count_ptts + income;
-        // checkAndChangeImage(count_ptts)
+        checkAndChangeImage(count_ptts)
         text_count_ptts.textContent = "🥔 " + count_ptts.toString() + " 🥔";
         localStorage.setItem('count_ptts', parseInt(count_ptts));
 
@@ -50,17 +50,17 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
 
-    // function checkAndChangeImage(count) {
-    //     if (count == 1488) {
-    //         ptt_btn.src = "images/webp/pashalko.webp";
-    //     } else {
-    //         let options_array = JSON.parse(localStorage.getItem('options_array')) || {};
-    //         keys = Object.keys(options_array)
-    //         lastKey=keys[keys.length - 1]
-    //         console.log(lastKey)
-    //         ptt_btn.src = "images/webp/" + lastKey + "_logo.webp";
-    //     }
-    // }
+    function checkAndChangeImage(count) {
+        if (count == 1488) {
+            ptt_btn.src = "images/webp/pashalko.webp";
+        } else {
+            let options_array = JSON.parse(localStorage.getItem('options_array')) || {};
+            keys = Object.keys(options_array)
+            lastKey = keys[keys.length - 1]
+            console.log(lastKey)
+            ptt_btn.src = "images/webp/" + lastKey + "_logo.webp";
+        }
+    }
 
     ptt_btn.addEventListener("mousedown", function () {
         ptt_btn.classList.remove("release_ptt_btn");
