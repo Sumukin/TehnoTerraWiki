7d01e01896abcf2f7b9eb69077faa46cc4ccc9a8
Sumukin <95268190+Sumukin@users.noreply.github.com>
2024-06-18 14:39:50 +0700

    Update on_page_load.js

--- PotatoClicker/scripts/on_page_load.js
+++ PotatoClicker/scripts/on_page_load.js
@@ -3,16 +3,10 @@ import {updatePttsIncome} from "./text_update.js";
 import {updatePttsCount} from "./text_update.js";
 import {checkAndChangeImage} from "./text_update.js"
 
-function UpdateIncomePtts() {
-    const text_income_ptts = document.getElementById("text_income_ptts");
-    let income_ptts = localStorage.getItem('income') ? parseInt(localStorage.getItem('income'), 10) : 0;
-    text_income_ptts.textContent = "+" + income_ptts.toString() + " 🥔/Клик";
-}
-
-
 document.addEventListener('DOMContentLoaded', function () {
+    var count_ptts = parseInt(localStorage.getItem('count_ptts') ? parseInt(localStorage.getItem('count_ptts'), 10) : 0);
     updatePttsCount();
     updatePttsIncome();
-    checkAndChangeImage(count_ptts)
+    checkAndChangeImage(count_ptts);
     updateAllBuyBtnsStatus();
 });
